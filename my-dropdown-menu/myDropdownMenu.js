1ff4ca26cd9cccdcd74121d7082b06923c823c12
RedHornetStudio <51362978+RedHornetStudio@users.noreply.github.com>
2021-01-02 16:17:23 +0200

    modified:   hide-outline-on-click/style.css new file:   hide-outline-on-click/style.css.map new file:   hide-outline-on-click/style.scss modified:   my-checkbox/style.css new file:   my-checkbox/style.css.map new file:   my-checkbox/style.scss modified:   my-dropdown-menu/index.html modified:   my-dropdown-menu/myDropdownMenu.js modified:   my-dropdown-menu/style.css new file:   my-dropdown-menu/style.css.map new file:   my-dropdown-menu/style.scss

--- my-dropdown-menu/myDropdownMenu.js
+++ my-dropdown-menu/myDropdownMenu.js
@@ -1,3 +1,4 @@
+///////////// with selected option
 const myDropdownMenu = document.querySelector('.my-dropdown-menu');
 const selected = myDropdownMenu.querySelector('.selected');
 const options = myDropdownMenu.querySelector('.options');
@@ -100,3 +101,93 @@ options.addEventListener('mouseover', e => {
 options.addEventListener('click', e => {
   selected.textContent = activeOption.textContent;
 });
+
+// ///////////// without selected option
+// const myDropdownMenu = document.querySelector('.my-dropdown-menu');
+// const selected = myDropdownMenu.querySelector('.selected');
+// const options = myDropdownMenu.querySelector('.options');
+// const optionsList = options.querySelectorAll('.option');
+
+// let activeOption = null;
+// myDropdownMenu.addEventListener('click', () => {
+//   if(options.classList.contains('show')) {
+//     options.classList.remove('show');
+//   } else {
+//     options.classList.add('show');
+//     optionsList.forEach(option => {
+//       option.classList.remove('active-option');
+//     });
+//     activeOption = null;
+//   }
+// });
+
+// myDropdownMenu.addEventListener('keyup', e => {
+//   if(e.key === 'Enter' || e.key === ' ') {
+//     if(options.classList.contains('show')) {
+//       options.classList.remove('show');
+//       if(activeOption) {
+//         console.log(`action from ${activeOption.textContent}`);
+//       }
+//     } else {
+//       options.classList.add('show');
+//       optionsList.forEach(option => {
+//         option.classList.remove('active-option');
+//       });
+//       activeOption = null;
+//     }
+//   }
+// });
+
+// myDropdownMenu.addEventListener('keydown', e => {
+//   if(e.key === ' ') {
+//     e.preventDefault();
+//   }
+//   if(e.key === 'ArrowDown') {
+//     e.preventDefault();
+//     optionsList.forEach(option => {
+//       option.classList.remove('active-option');
+//     });
+//     if(activeOption && activeOption.nextElementSibling) {
+//       activeOption = activeOption.nextElementSibling;
+//     } else {
+//       activeOption = optionsList[0]
+//     }
+//     activeOption.classList.add('active-option');
+//   }
+//   if(e.key === 'ArrowUp') {
+//     e.preventDefault();
+//     optionsList.forEach(option => {
+//       option.classList.remove('active-option');
+//     });
+//     if(activeOption && activeOption.previousElementSibling) {
+//       activeOption = activeOption.previousElementSibling;
+//     } else {
+//       activeOption = optionsList[optionsList.length - 1];
+//     }
+//     activeOption.classList.add('active-option');
+//   }
+// });
+
+// myDropdownMenu.addEventListener('focusout', () => {
+//   options.classList.remove('show');
+// });
+
+// options.addEventListener('mousemove', e => {
+//     optionsList.forEach(option => {
+//       option.classList.remove('active-option');
+//     });
+//     activeOption = e.target;
+//     activeOption.classList.add('active-option');
+// });
+
+// options.addEventListener('mouseover', e => {
+//   optionsList.forEach(option => {
+//     option.classList.remove('active-option');
+//   });
+//   activeOption = e.target;
+//   activeOption.classList.add('active-option');
+// });
+
+// options.addEventListener('click', e => {
+//   console.log(`action from ${activeOption.textContent}`);
+// });
